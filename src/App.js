6aff1a8dceb4d7c9c8367c2dc7589f004d688925
engkos <engkoskostaman759@gmail.com>
2022-12-07 12:19:35 +0700

    crud json-server

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     super(props);
     this.state = {
       dataApi: [],
+      edit: false,
       dataPost: {
         id: 0,
         title: '',
@@ -14,28 +15,47 @@ class App extends Component {
     };
     this.handleRemove = this.handleRemove.bind(this);
     this.inputChange = this.inputChange.bind(this);
-    this.onSubmitForm = this.onSubmitForm.bind(this);
+
   }
 
   reloadData() {
     axios.get('http://localhost:3004/posts').then(
       res => {
         this.setState({
-          dataApi: res.data
+          dataApi: res.data,
+          edit: false
         })
       }
     );
   }
-
-  onSubmitForm() {
-    axios.post('http://localhost:3004/posts', this.state.dataPost).then(() => {
-      this.reloadData();
-    });
+  clearData = () => {
+    let newdataPost = { ...this.state.dataPost };
+    newdataPost['id'] = "";
+    newdataPost['body'] = "";
+    newdataPost['title'] = "";
+    this.setState({
+      dataPost: newdataPost
+    })
   }
+  onSubmitForm = () => {
+    if (this.state.edit === false) {
+      axios.post('http://localhost:3004/posts', this.state.dataPost).then(() => {
+        this.reloadData();
+        this.clearData();
+      });
+    } else {
+      axios.put(`http://localhost:3004/posts/${this.state.dataPost.id}`, this.state.dataPost).then(() => {
+        this.reloadData();
+        this.clearData();
+      })
+    }
+  };
 
   inputChange(e) {
     let newdataPost = { ... this.state.dataPost };
-    newdataPost['id'] = new Date().getTime();
+    if (this.state.edit === false) {
+      newdataPost['id'] = new Date().getTime();
+    }
     newdataPost[e.target.name] = e.target.value;
 
     this.setState({
@@ -44,19 +64,21 @@ class App extends Component {
     );
   }
 
+  getDataId = e => {
+    axios.get(`http://localhost:3004/posts/${e.target.value}`).then(res => {
+      this.setState({
+        dataPost: res.data,
+        edit: true
+      })
+    });
+  }
+
   handleRemove(e) {
     console.log(e.target.value);
     fetch(`http://localhost:3004/posts/${e.target.value}`, { method: "DELETE" }).then(res =>
       this.reloadData());
   }
   componentDidMount() {
-    // fetch('https://jsonplaceholder.typicode.com/posts')
-    //   .then(response => response.json())
-    //   .then(res => {
-    //     this.setState({
-    //       dataApi: res
-    //     })
-    //   })
     this.reloadData();
 
   }
@@ -64,14 +86,24 @@ class App extends Component {
     return (
       <div>
         <p>Hello Api</p>
-        <input type='text' name='body' placeholder='Masukan Body' onChange={this.inputChange} />
-        <input type='text' name='title' placeholder='Masukan Title' onChange={this.inputChange} />
-        <button type='submit' onClick={this.onSubmitForm}>Add Data</button>
+        <input type='text'
+          name='body'
+          value={this.state.dataPost.body}
+          placeholder='Masukan Body'
+          onChange={this.inputChange} />
+        <input type='text'
+          name='title'
+          value={this.state.dataPost.title}
+          placeholder='Masukan Title'
+          onChange={this.inputChange} />
+        <button type='submit' onClick={this.onSubmitForm}>Save Data</button>
         {this.state.dataApi.map((dat, index) => {
           return (
             <div key={index}>
+              <p>{dat.body}</p>
               <p>{dat.title}</p>
               <button value={dat.id} onClick={this.handleRemove}>Delete</button>
+              <button value={dat.id} onClick={this.getDataId}>Edite</button>
             </div>
           );
         })}
